7718ef66f4dcdab0f287299c25b6b553d34554d1
Yoncho <yoncho.yonchrev@p3-group.com>
2023-11-09 15:45:26 +0200

    fix sql

--- Initial.sql
+++ Initial.sql
@@ -1,15 +1,13 @@
 CREATE DATABASE test123
 USE test123
 
-drop table building
-
 CREATE TABLE [building] (
     [BuildingId] uniqueidentifier NOT NULL,
     [City] nvarchar(85) NOT NULL,
     [StreetAddress] nvarchar(255) NOT NULL,
     [Neighbourhood] nvarchar(100) NOT NULL,
-    [Floors] int NOT NULL DEFAULT 1
+    [Floors] int NOT NULL
 );
 ALTER TABLE [building] ADD	CONSTRAINT PK_building PRIMARY KEY([BuildingId]);
 
-DELETE FROM building;
+ALTER TABLE [building] ADD CONSTRAINT DF_Floors DEFAULT 1 FOR [Floors];
